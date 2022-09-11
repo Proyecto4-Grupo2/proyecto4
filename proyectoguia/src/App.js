b36e8e1be73f3913e6c26db88f224192af9d2cc1
Cristal <cristal_lenny@hotmail.com>
2022-09-11 18:16:08 +0200

    cadrs equipo

--- proyectoguia/src/App.js
+++ proyectoguia/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Cards from './components/Cards';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards 
+        imagen='ainara'
+        nombre='Ainara Arbiol'
+        cargo='Dto de programción y sistemas'/>
+      <Cards 
+        imagen='angelica'
+        nombre='Angélica'
+        cargo='Dto de programción y sistemas'/>
+      <Cards 
+        imagen='barbara'
+        nombre='Bárbara Santano'
+        cargo='Dto de programción y sistemas'/>
+      <Cards 
+        imagen='cristal'
+        nombre='Cristal Lightbourn'
+        cargo='Dto de programción y sistemas'/>
+      <Cards 
+        imagen='karina'
+        nombre='Karina Perdomo'
+        cargo='Dto de programción y sistemas'/>
+      <Cards 
+        imagen='marcela'
+        nombre='Marcela Blanco'
+        cargo='Dto de programción y sistemas'/>
+        <Cards 
+          imagen='maria'
+          nombre='María Martínez'
+          cargo='Dto de programción y sistemas'/>
     </div>
+    
   );
 }
 

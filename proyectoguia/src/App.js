a87c58c3cc40d8de9a9a3a14ec54e0f2aa8be874
blanco <mblancospadaro@gmail.com>
2022-09-12 20:27:18 +0200

    tarjetas

--- proyectoguia/src/App.js
+++ proyectoguia/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './style.css';
+import data from "./TemplateData.json";
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="templateContainer">
+        <div className="searchInput_Container">
+          <input id="searchInput" type="text" placeholder="Busqueda..." onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }} />
+        </div>
+        <div className="template_Container">
+          {
+            data 
+              .filter((val) => {
+                if(searchTerm === ""){
+                  return val;
+                }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                  return val;
+                }else if(val.distrito.toLowerCase().includes(searchTerm.toLowerCase())){
+                  return val;
+                }else if(val.address.toLowerCase().includes(searchTerm.toLowerCase())){
+                  return val; 
+                  
+                }})
+              .map((val) => {
+                return(
+                  <div className="template" key={val.id}>
+                 
+                  <img src={val.image} alt="" /> 
+                  
+                      <h3>{val.title}</h3>
+                     
+                      <p>Dirección: {val.address}</p>
+                    
+                      <p>Distrito: {val.distrito}</p>
+                      <p>{val.descripcion}</p>
+                      <a href={val.enlace}> Ir a la web</a>
+                  </div> 
+                )
+              })
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
+
